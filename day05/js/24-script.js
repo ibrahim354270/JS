f7499daaf35fb04c4f239627f0d43da52812471d
İbrahim Sağlam <saglam.354270@gmail.com>
2024-02-02 23:52:24 +0300

    02.02.22

---
+++ day05/js/24-script.js
@@ -0,0 +1,26 @@
+
+
+    function changeMode(){
+
+       let bodyEl=document.querySelector("body")
+       let buttonToggle= document.querySelector(".toggle-btn")
+
+    let isDark=   bodyEl.classList.contains("dark")
+   // console.log(isDark)
+
+   if(isDark){
+    bodyEl.classList.remove("dark")
+  //  bodyEl.classList.toggle("dark")
+
+    buttonToggle.innerHTML=`<i class="fa-solid fa-moon"></i>`
+
+   }else{
+
+    bodyEl.classList.add("dark")
+    buttonToggle.innerHTML=`<i class="fa-solid fa-"></i>`
+
+
+   }
+
+
+}
