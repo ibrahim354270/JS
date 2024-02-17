b687340660ae4af17732afe588d6560e3856c911
İbrahim Sağlam <saglam.354270@gmail.com>
2024-02-17 23:27:29 +0300

    17.02.24

--- day15/js/75-script.js
+++ day15/js/75-script.js
@@ -1,20 +1,31 @@
-import {students} from "../data/students.js";
-
+import { students } from "../data/students.js";
+document.querySelector("#btnShow").addEventListener("click", () => {
+    let pointEl = document.querySelectorAll(
+        "#tblStudents  tbody tr td:last-child"
+    );
+    //console.log(pointEl)
+    pointEl.forEach((point, index) => {
+        let pointText = point.innerText;
+        if (pointText < 50) {
+            document
+                .querySelector(`#tblStudents  tbody tr:nth-child(${index + 1})`)
+                .classList.add("table-danger");
+        }else{
+            document
+                .querySelector(`#tblStudents  tbody tr:nth-child(${index + 1})`)
+                .classList.add("table-success");
+        }
+    });
+});
 const loadData = () => {
-    
-    let listEl ="";
-    students.forEach((student, index)=>{
-
-        listEl += 
-        ` <tr>
-        <td>${index+1}</td>
-        <td>${student.name}</td>
-        <td>${student.point}</td>
-        </tr>` 
-
-        
-    })
-
-    document.querySelector("#tblStudents tbody").innerHTML = listEl
-}
+    let listEl = "";
+    students.forEach((student, index) => {
+        listEl += `    <tr>
+    <td>${index + 1}</td>
+    <td>${student.name}</td>
+    <td>${student.point}</td>
+</tr>`;
+    });
+    document.querySelector("#tblStudents tbody").innerHTML = listEl;
+};
 loadData();
